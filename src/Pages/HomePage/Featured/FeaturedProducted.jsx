8cddaedad38047808ba2977ecc2df9c67ca14a3a
Rayhan <mdrayhaanahmed@gmail.com>
2024-12-28 19:34:34 +0600

    design the featured product section

--- src/Pages/HomePage/Featured/FeaturedProducted.jsx
+++ src/Pages/HomePage/Featured/FeaturedProducted.jsx
@@ -1,40 +1,83 @@
-import { useState, useEffect} from "react"
-import Featured from "../../../component/Navber/Featured"
-
+import { useState, useEffect } from "react";
+import Featured from "../../../component/Navber/Featured";
+import image from "../../../assets/featured-1-565x640.png";
 
 export default function FeaturedProducted() {
+  const [featuredData, setFeatured] = useState([]);
 
-  const [featuredData, setFeatured] = useState([])
+  useEffect(() => {
+    fetch("/data.json")
+      .then((res) => res.json())
+      .then((data) => setFeatured(data));
+  }, []);
 
-   useEffect(() => {
-    fetch('/data.json')
-    .then((res) => res.json())
-    .then((data) => setFeatured(data))
-
-  }, [])
-
-  
-  
   return (
     <div className="my-16">
-        <div className="text-center">
-            <h2 className="text-4xl font-semibold">Shop The Featured Products</h2>
-            <p className="mt-4 text-[#01162DAB] text-[16px]"> Whether you&apos;re looking to treat yourself or surprise someone special, makes every occasion memorable.</p>
-        </div>
+      <div className="text-center">
+        <h2 className="text-4xl font-semibold">Shop The Featured Products</h2>
+        <p className="mt-4 text-[#01162DAB] text-[16px]">
+          {" "}
+          Whether you&apos;re looking to treat yourself or surprise someone
+          special, makes every occasion memorable.
+        </p>
+      </div>
 
-        {/* featured products */}
-        <div>
-            {/* top featured producted */}
-            <div>
-
+      {/* featured products */}
+      <div className="mt-12 md:mx-8 flex gap-8">
+        {/* top featured producted */}
+        <div
+          className="bg-cover lg:h-[652px] md:w-2/6 bg-center relative"
+          style={{
+            backgroundImage: `url(${image})`,
+          }}
+        >
+          <h3 className="bg-[#A31352] text-white absolute top-5 left-5 py-2 px-3">
+            Top Featured
+          </h3>
+          <div className="absolute bottom-7 left-7">
+            <h2 className="text-[18px] uppercase font-semibold tracking-[1.08px]">
+              Pink Papaercut Gift Box
+            </h2>
+            {/* rating section start */}
+            <div className="rating rating-sm mt-3">
+              <input
+                type="radio"
+                name="rating-2"
+                className="mask mask-star-2 bg-[#A31352]"
+              />
+              <input
+                type="radio"
+                name="rating-2"
+                className="mask mask-star-2 bg-[#A31352]"
+                defaultChecked
+              />
+              <input
+                type="radio"
+                name="rating-2"
+                className="mask mask-star-2 bg-[#A31352]"
+              />
+              <input
+                type="radio"
+                name="rating-2"
+                className="mask mask-star-2 bg-[#A31352]"
+              />
+              <input
+                type="radio"
+                name="rating-2"
+                className="mask mask-star-2 bg-[#A31352]"
+              />
             </div>
-            {/* others products */}
-            <div>
-                {
-                  featuredData.map((featured) => <Featured key={featured.id} featured = {featured}></Featured>)
-                }
-            </div>
+            {/* rating section end */}
+            <p className="text-[18px] mt-2">$30.50</p>
+          </div>
         </div>
+        {/* others products */}
+        <div className="grid lg:grid-cols-2 grid-cols-1 gap-6 lg:w-4/6">
+          {featuredData.map((featured) => (
+            <Featured key={featured.id} featured={featured}></Featured>
+          ))}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/component/Navber/Featured.jsx
+++ src/component/Navber/Featured.jsx
@@ -1,8 +1,23 @@
+import { BsCart2 } from "react-icons/bs";
 
-
-export default function Featured({featured}) {
-    const {heading} = featured;
+export default function Featured({ featured }) {
+  const { heading, image, price } = featured;
   return (
-    <div>{heading}</div>
-  )
+    <div className="flex items-center gap-6 card-side bg-base-100 transform  transition-all duration-500 py-4 px-4 border border-gray-200 hover:shadow-lg">
+      <figure>
+        <img className="w-[148px] h-[163px]" src={image} alt="Movie" />
+      </figure>
+      <div style={{ width: "calc(100% - 168px)" }}>
+        <h2 className="text-[16px] tracking-[0.96px] leading-6 font-semibold uppercase">
+          {heading}
+        </h2>
+        <p className="mt-2">${price}</p>
+        <div className="flex justify-end">
+          <button className="bg-[#7d65ae] mt-3 text-white py-3 px-3 rounded-full">
+            <BsCart2 className="text-xl" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
